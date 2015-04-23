a1ee5780278f9341e3973bf1cce5ea517faf1db8
Bilal Çınarlı <bcinarli@gmail.com>
2015-04-23 13:24:41 +0300

    Initial commit

---
+++ app/pages/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Bilal Cinarli
+ * @link http://bcinarli.com
+ **/
+
+html::$id = 'homepage';
+html::$class = 'homepage';
+
+?>
+<?php tools::inc('includes/header'); ?>
+<?php sample::helloWorld(); ?>
+<?php tools::inc('includes/footer');
