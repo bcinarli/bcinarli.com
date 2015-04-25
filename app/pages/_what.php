a6861cb89a812a36137f3db27085e9d07408dc48
Bilal Çınarlı <bcinarli@gmail.com>
2015-04-25 18:59:09 +0300

    Update: What I'm Doing

--- app/pages/_what.php
+++ app/pages/_what.php
@@ -8,6 +8,11 @@
 <section id="what" class="page-section">
     <article class="section-content content-holder">
         <h2 class="section-title"><i class="icon-lab"></i> What I'm Doing?</h2>
-        <p>I build <strong>frameworks</strong>, create <strong>future proof</strong> front-end systems</p>
+
+        <p>I'm a big enthusiast of building <strong>maintainable</strong> and <strong>scalable</strong> front-ends. To achieve this goal, I <strong>build</strong> frameworks mostly power with Sass, PHP and love. I <strong>maintain</strong> open-source projects such as <a href="https://github.com/bcinarli/caffeine" title="Sass based Mixin and Functions library">Caffeine</a>, <a href="https://github.com/bcinarli/melange" title="A maintainable CSS architecture approach">Melange</a>, <a href="https://github.com/bcinarli/misto" title="A PHP based prototyping and blogging framework">Misto</a> and <a href="https://github.com/uxrocket" title="A set of jQuery plugins and JavaScript methods for interactive sites">UX Rocket</a></p>
+        
+        <p>In day-to-day works, I code <strong>mobile web apps</strong> and <strong>JavaScript applications</strong> for intranets and as well as clients. I <strong>manage</strong> teams and <strong>optimize</strong> the front-end development approaches through different development teams. Also, I <strong>teach</strong> HTML/CSS and JavaScript techniques and gave workshops about Front-end related topics.</p>
+
+        <p>I <strong>blog</strong> at <a href="http://htmlmag.com">HTML Mag</a> and share my thoughts about web development, mobile approaches. In HTML Mag, we organize events, talks, workshops...</p>
     </article>
 </section>
