9e7c029ce07229728f5f0548e076f377ea0a7d99
Bilal Çınarlı <bcinarli@gmail.com>
2015-04-26 00:34:06 +0300

    New: footer, copyright

--- app/pages/includes/footer.php
+++ app/pages/includes/footer.php
@@ -6,6 +6,11 @@
 ?>		
 		</div><!-- #main -->
 	</div><!-- #wrapper -->
+    <footer class="page-footer">
+        <div class="content-holder">
+            <p>© <?php echo date('Y'); ?> - Bilal Cinarli. Built with <a href="https://github.com/bcinarli/misto">Misto</a> and Love. Cover image by <a href="http://sertouch.com" target="_blank">Sertaç Piskin</a>.</p>
+        </div>
+    </footer>
     <script src="<?php echo url::scripts('vendors/jquery.min.js'); ?>"></script>
     <script src="<?php echo url::scripts('app.js'); ?>"></script>
     <script type="text/javascript" src="http://include.reinvigorate.net/re_.js"></script>
