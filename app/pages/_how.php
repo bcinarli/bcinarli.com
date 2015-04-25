c89b1620b1b1615230ed74db0599bed1276de19c
Bilal Çınarlı <bcinarli@gmail.com>
2015-04-25 19:07:10 +0300

    Update: How I'm Doing

--- app/pages/_how.php
+++ app/pages/_how.php
@@ -8,6 +8,13 @@
 <section id="how" class="page-section">
     <article class="section-content content-holder">
         <h2 class="section-title"><i class="icon-ruler"></i> How I'm Doing?</h2>
-        <p>I use HTML5, CSS, Sass, JavaScript, jQuery, Backbone, Angular, PHP</p>
+
+        <p>In front-end development, I use <strong>HTML5</strong>, <strong>CSS/Sass</strong> and <strong>JavaScript/jQuery</strong>, well mostly. I create my own jQuery plugin when I need one. For a web app and mobile/tablet app, I generally prefer <strong>Backbone</strong> and for a form-based application I use <strong>Angular</strong>.</p>
+
+        <p>When I need a backend related stuff, I use <strong>PHP</strong> and along with <strong>MySQL</strong>. I can write a custom <strong>CMS</strong> or a <strong>WordPress</strong> theme/plugin for project needs.</p>
+
+        <p>In order to automate development, I'm using <strong>Codekit</strong> and <strong>Grunt</strong>. Keeping my tasks with Basecamp and Github.</p>
+
+        <p>I like to <strong>try new technologies</strong> and make my hands dirty. I check new frameworks, coding styles and approaches as something new come up.</p>
     </article>
 </section>
