91f9b1c00da25bb9338a20365e3bdae0bf7a47e0
Bilal Çınarlı <bcinarli@gmail.com>
2015-04-26 11:14:21 +0300

    Change: reinvigorate removed

--- app/pages/includes/footer.php
+++ app/pages/includes/footer.php
@@ -13,12 +13,6 @@
     </footer>
     <script src="<?php echo url::scripts('vendors/jquery.min.js'); ?>"></script>
     <script src="<?php echo url::scripts('app.js'); ?>"></script>
-    <script type="text/javascript" src="http://include.reinvigorate.net/re_.js"></script>
-    <script type="text/javascript">
-        try {
-            reinvigorate.track("1avrh-pz920q730t");
-        } catch(err) {}
-    </script>
     <script type="text/javascript">
         var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
         document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
