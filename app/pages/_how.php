a973a3a8a76cf5bafa11ab0ca4ffd314102ead30
Bilal Cinarli <bcinarli@gmail.com>
2016-10-13 07:57:55 +0300

    How?

--- app/pages/_how.php
+++ app/pages/_how.php
@@ -6,15 +6,21 @@
 ?>
 <!-- how i'm doing -->
 <div id="how" class="page-section">
-    <article class="section-content content-holder">
-        <h2 class="section-title"><i class="icon-ruler"></i> How I'm Doing?</h2>
+	<article class="section-content content-holder">
+		<h2 class="section-title"><i class="icon-ruler"></i> How I'm Doing?</h2>
 
-        <p>In front-end development, I use <strong>HTML5</strong>, <strong>CSS/Sass</strong> and <strong>JavaScript/jQuery</strong>, well mostly. I create my own jQuery plugin when I need one. For a web app and mobile/tablet app, I generally prefer <strong>Backbone</strong> and for a form-based application I use <strong>Angular</strong>.</p>
+		<p>In front-end development, I use <strong>HTML5</strong>, <strong>CSS/Sass</strong> and <strong>ES6/JavaScript/React/Backbone/jQuery</strong>
+			or whatever I need. I create my own jQuery plugin when I need one. When I need more robust application I use
+			ES6. For simple views and small applications I prefer Backbone. When building advanced
+			applications I use <strong>ES6 & React & Redux</strong> for now...</p>
 
-        <p>When I need a backend related stuff, I use <strong>PHP</strong> and along with <strong>MySQL</strong>. I can write a custom <strong>CMS</strong> or a <strong>WordPress</strong> theme/plugin for project needs.</p>
+		<p>When I need a backend related stuff, I use <strong>PHP</strong> and along with <strong>MySQL</strong>. I can
+			write a custom <strong>CMS</strong> or a <strong>WordPress</strong> theme/plugin for project needs.</p>
 
-        <p>In order to automate development, I'm using <strong>Codekit</strong> and <strong>Grunt</strong>. Keeping my tasks with Basecamp and Github.</p>
+		<p>In order to automate development, I'm using <strong>Gulp</strong> or <strong>Grunt</strong> or
+			<strong>Webpack</strong>. Keeping my tasks with Basecamp and store my codes on Github.</p>
 
-        <p>I like to <strong>try new technologies</strong> and make my hands dirty. I check new frameworks, coding styles and approaches as something new come up.</p>
-    </article>
+		<p>I like to <strong>try new technologies</strong> and make my hands dirty. I check new frameworks, coding
+			styles and approaches as something new come up.</p>
+	</article>
 </div>
