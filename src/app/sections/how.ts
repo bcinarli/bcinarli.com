d19ee7696da70a2c7c6a06dabd8e457942b0d828
Bilal Cinarli <bcinarli@gmail.com>
2023-02-21 13:42:07 +0100

    update how I'm doing

--- src/app/sections/how.ts
+++ src/app/sections/how.ts
@@ -1,5 +1,5 @@
-import { html } from "../../lib/renderer";
-import Wave from "../icons/wave";
+import { html } from '../../lib/renderer';
+import Wave from '../icons/wave';
 
 const How = () => {
   return html`
@@ -8,32 +8,47 @@ const How = () => {
         <h2 class="section-title">${Wave()} How I'm Doing?</h2>
 
         <p>
-          In frontend development, I use <strong>HTML5</strong>,
-          <strong>CSS/Sass</strong> and
-          <strong>ES6/ JavaScript/ React/ Backbone/ jQuery</strong> or whatever I
-          need. I create my own jQuery plugin when I need one. When I need more
-          robust application I use ES6. For simple views and small applications
-          I prefer Backbone. When building advanced applications I use
-          <strong>ES6 & React & Redux</strong> for now...
+          I <strong>read</strong> a lot to keep my skills up to date. I love to <strong>learn</strong> new things and
+          <strong>experiment</strong> with new approaches. I believe in <strong>fail fast</strong>,
+          <strong>fail beautiful</strong>, <strong>tweak</strong> and <strong>restart</strong> approach.
         </p>
 
         <p>
-          When I need a backend related stuff, I use <strong>PHP</strong> and
-          along with <strong>MySQL</strong>. I can write a custom
-          <strong>CMS</strong> or a <strong>WordPress</strong> theme/plugin for
-          project needs.
+          I provide <strong>guided</strong> flexibility and a <strong>safe</strong> environment to the teams I manage. I
+          <strong>listen</strong> to the engineers and what they are keen on building. Then, I
+          <strong>encourage</strong> them to create initiatives towards those and <strong>fine-tune</strong> them to
+          overlap with company <strong>OKRs</strong>and targets.
         </p>
 
         <p>
-          In order to automate development, I'm using <strong>Gulp</strong> or
-          <strong>Grunt</strong> or <strong>Webpack</strong>. Keeping my tasks
-          with Basecamp and store my codes on Github.
+          Even though I have vast expertise in <strong>frontend development</strong> and
+          <strong>developer tooling</strong>, I have a good understanding and hands-on experience in
+          <strong>backend</strong>, <strong>cloud</strong>, <strong>DevOps</strong> and <strong>databases</strong>.
         </p>
 
         <p>
-          I like to <strong>try new technologies</strong> and make my hands
-          dirty. I check new frameworks, coding styles and approaches as
-          something new come up.
+          Those skills help me to understand the end-to-end flow and navigate through unknowns. That helped me find
+          opportunities to <strong>improve</strong> application <strong>performance</strong>, developer
+          <strong>experience</strong> and <strong>business</strong>. For example, while working at adidas in 2021, I
+          contributed an <strong>80 million Euro uplift</strong> in online sales by seeing an opportunity to improve
+          page speed after <strong>analysing the user/visit analytic</strong>s data and
+          <strong>browser statistics</strong>.
+        </p>
+
+        <p>
+          Since 2001, I have used different stacks in the frontend, from <strong>Prototype</strong> to
+          <strong>MooTools</strong> to <strong>jQuery</strong>, <strong>Backbone</strong> to
+          <strong>Angular</strong> and <strong>React</strong>, and <strong>TypeScript</strong> and
+          <strong>Node</strong>. For styling, I used different approaches like <strong>utilities</strong> to
+          <strong>BEM</strong> and <strong>OOCSS</strong>, pure CSS to preprocessors like <strong>Less</strong> and
+          <strong>Sass</strong>, <strong>PostCSS</strong>, <strong>CSS-in-JS</strong>, and <strong>CSS Modules</strong>.
+          On the tooling side, from ready-made tools to <strong>Browserify</strong>, <strong>Grunt</strong> and
+          <strong>Gulp</strong> to <strong>Webpack</strong> and then <strong>esbuild</strong>, <strong>Vite</strong>.
+          Not to mention testing with <strong>Mocha</strong>, <strong>Chai</strong>, <strong>Istanbul</strong>,
+          <strong>Jest</strong>, and libraries like <strong>Enzyme</strong>, <strong>Testing Library</strong> and
+          linting the code with <strong>jshint</strong>, <strong>eslint</strong>, <strong>stylelint</strong>,
+          <strong>prettier</strong>... However, I do not have a strong preference for the tools. Whatever works best for
+          the team is more important.
         </p>
       </article>
     </div>
--- src/app/sections/what.ts
+++ src/app/sections/what.ts
@@ -9,13 +9,13 @@ const What = () => {
 
         <p>
           I'm a big enthusiast of building <strong>maintainable</strong> and <strong>scalable</strong> frontends. To
-          achieve this goal; I <strong>analyse</strong> the problems, <strong>observe</strong> the business changes,
+          achieve this goal, I <strong>analyse</strong> the problems, <strong>observe</strong> the business changes,
           <strong>partner</strong> with product owners to <strong>define</strong> technology roadmaps and look for
           opportunities.
         </p>
 
         <p>
-          I deal with <strong>architectural decisions</strong>, PoCs, enabling engineers to
+          I deal with <strong>architectural decisions</strong> and PoCs. I work to enable engineers to
           <strong>perform without blockers</strong>. Also, to improve <strong>developer experience</strong>, I deal with
           <strong>developer tooling</strong>, analyse the pain points and work to improve the local development
           environment, developer happiness and productivity.
@@ -23,14 +23,15 @@ const What = () => {
 
         <p>
           I help to <strong>grow</strong> my team both in technical and in their soft skills. I regularly
-          <strong>mentor</strong> junior engineers and <strong>coach</strong> senior / staff engineers. Throughout my
-          career, I <strong>hired</strong> engineers, <strong>built high-performing teams</strong>, and managed team
-          harmony and growth.
+          <strong>mentor</strong> junior engineers and <strong>coach</strong> senior / staff engineers. In addition to
+          this, I <strong>hired</strong> engineers, <strong>built high-performing teams</strong>, and managed team
+          harmony and growth throughout my career
         </p>
 
         <p>
-          To deliver the goals better, depending on the needs and requirements, I built web <strong>frameworks</strong>, created <strong>design
-          systems</strong>, designed <strong>frontend architectures</strong>, taught modern web development and gave workshops.
+          To deliver the goals better, depending on the needs and requirements, I built web <strong>frameworks</strong>,
+          created <strong>design systems</strong>, designed <strong>frontend architectures</strong>, taught modern web
+          development and gave workshops.
         </p>
       </article>
     </div>
